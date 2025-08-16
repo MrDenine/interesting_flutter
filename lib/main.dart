5797713a496397cce8460264130f0c1ea22dd359
PHANUWAT\panuw <Panuwat_nine@hotmail.com>
2025-08-16 17:27:01 +0700

    ADD firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,14 @@ import 'routes/app_routes.dart';
 import 'routes/route_transitions.dart';
 import 'core/services/navigation/navigation_service.dart';
 import 'core/providers/app_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: InterestingFlutterApp(),
